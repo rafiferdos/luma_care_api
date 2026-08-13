950b41ef81e21ccb31c2e245fb37bd4c2b890f51
Rafi Ferdos <rafiferdos@gmail.com>
2026-08-13 23:18:00 +0600

    feat: refactor app.ts for improved readability and update biome.json configuration

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,38 @@
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import express, {
-	type Application,
-	type Request,
-	type Response,
-} from "express";
-import httpStatus from "http-status";
-import config from "./app/config";
-import globalErrorHandler from "./app/middlewares/globalErrorHandler";
-import notFound from "./app/middlewares/notFound";
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import express, { type Application, type Request, type Response } from 'express'
+import httpStatus from 'http-status'
+import config from './app/config'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import notFound from './app/middlewares/notFound'
 
-const app: Application = express();
+const app: Application = express()
 
 app.use(
-	cors({
-		origin: config.frontend_url,
-		credentials: true,
-	}),
-);
+  cors({
+    origin: config.frontend_url,
+    credentials: true
+  })
+)
 
 // Enable URL-encoded form data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 // Middleware to parse JSON bodies
-app.use(express.json());
-app.use(cookieParser());
+app.use(express.json())
+app.use(cookieParser())
 
 // app.use("/api/v1/auth", AuthRoutes);
 
 // Basic route
-app.get("/", async (req: Request, res: Response) => {
-	res.status(httpStatus.OK).json({
-		success: true,
-		message: "Welcome to PH Healthcare System Backend",
-	});
-});
+app.get('/', async (req: Request, res: Response) => {
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: 'Welcome to PH Healthcare System Backend'
+  })
+})
 
-app.use(globalErrorHandler);
-app.use(notFound);
+app.use(globalErrorHandler)
+app.use(notFound)
 
-export default app;
+export default app
