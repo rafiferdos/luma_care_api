716ee6871af93c47aa2d7e64bfb7a66828b622f7
Rafi Ferdos <rafiferdos@gmail.com>
2026-08-18 21:58:36 +0600

    refactor: update role handling in auth routes and tidy up imports

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -1,18 +1,21 @@
 import { Router } from 'express'
 
-import { UserRole } from '@/generated/prisma/enums.js'
-import { auth } from '@/middlewares/auth.js'
+
 
 import { AuthControllers } from './auth.controller.js'
+import { UserRole } from '../../../../prisma/generated/prisma/browser.js';
+import { auth } from '@/app/middlewares/auth.js';
 
 const router = Router()
+
+const ALL_ROLES = [UserRole.ADMIN, UserRole.DOCTOR, UserRole.PATIENT, UserRole.SUPER_ADMIN]
 
 router.post('/login', AuthControllers.login)
 router.post('/refresh-token', AuthControllers.refreshToken)
 router.post('/register', AuthControllers.register)
 router.get(
   '/me',
-  auth(UserRole.ADMIN, UserRole.CUSTOMER, UserRole.TECHNICIAN),
+  auth(...ALL_ROLES),
   AuthControllers.getMe
 )
 router.post('/google', AuthControllers.googleLogin)
