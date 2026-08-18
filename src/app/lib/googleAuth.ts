d51b6ca5f4f7c606b24efcf4f6e8a7448f1db734
Rafi Ferdos <rafiferdos@gmail.com>
2026-08-18 18:43:46 +0600

    feat: add Google token verification functionality to authentication module

---
+++ src/app/lib/googleAuth.ts
@@ -0,0 +1,12 @@
+import { OAuth2Client } from 'google-auth-library'
+import config from '../config'
+
+const client = new OAuth2Client(config.google_client_id)
+
+export const verifyGoogleToken = async (idToken: string) => {
+  const ticket = await client.verifyIdToken({
+    idToken,
+    audience: config.google_client_id
+  })
+  return ticket.getPayload()
+}
