ca6945e8c4e76de562997e3a4760be9e89573693
Rafi Ferdos <rafiferdos@gmail.com>
2026-08-18 20:11:21 +0600

    refactor: tidy up imports and fix import path for PrismaClient

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,11 +1,8 @@
 import 'dotenv/config'
-
 import { PrismaPg } from '@prisma/adapter-pg'
-
-import { PrismaClient } from '../../../generated/prisma/client'
+import { PrismaClient } from '../../../prisma/generated/prisma/client';
 
 const connectionString = process.env.DATABASE_URL
-
 const adapter = new PrismaPg({ connectionString })
 
 const globalForPrisma = globalThis as unknown as {
