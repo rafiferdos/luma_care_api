1047232468ccf9f5a1131d1ba197307e88f3cfb7
Rafi Ferdos <rafiferdos@gmail.com>
2026-09-09 00:54:27 +0600

    feat: add unused-imports plugin to ESLint configuration and update rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,21 +1,45 @@
 import eslintConfigPrettier from 'eslint-config-prettier'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import unusedImports from 'eslint-plugin-unused-imports'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   {
-    ignores: ['dist/**', 'generated/**', 'node_modules/**']
+    ignores: [
+      'dist/**',
+      'generated/**',
+      'prisma/generated/**',
+      'node_modules/**'
+    ]
   },
+
   ...tseslint.configs.recommended,
+
   {
     plugins: {
-      'simple-import-sort': simpleImportSort
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports
     },
+
     rules: {
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
-      'unused-imports/no-unused-imports': 'warn',
+
+      '@typescript-eslint/no-unused-vars': 'off',
+
+      'unused-imports/no-unused-imports': 'error',
+
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_'
+        }
+      ]
     }
   },
+
   eslintConfigPrettier
 )
