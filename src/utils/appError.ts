73a821187af76af64a4ae5e7b985eb419b8b3e1f
Rafi Ferdos <rafiferdos@gmail.com>
2026-09-09 01:06:15 +0600

    refactor: update AppError class to improve type handling and message resolution

--- src/utils/appError.ts
+++ src/utils/appError.ts
@@ -1,32 +1,40 @@
 import { HTTPException } from 'hono/http-exception'
-import type {
-  ClientErrorStatusCode,
-  ServerErrorStatusCode
-} from 'hono/utils/http-status'
 
-export type AppErrorStatus = ClientErrorStatusCode | ServerErrorStatusCode
+import { getStatusMessage, type HttpErrorStatus } from './httpStatus'
 
 type AppErrorOptions = Readonly<{
   errors?: unknown
   cause?: unknown
 }>
 
+type MessageOrOptions = string | AppErrorOptions
+
 export class AppError extends HTTPException {
+  readonly statusCode: HttpErrorStatus
   readonly isOperational = true as const
   readonly errors?: unknown
 
   constructor(
-    status: AppErrorStatus,
-    message: string,
+    statusCode: HttpErrorStatus,
+    messageOrOptions?: MessageOrOptions,
     options: AppErrorOptions = {}
   ) {
-    super(status, {
+    const resolvedOptions =
+      typeof messageOrOptions === 'object' ? messageOrOptions : options
+
+    const message =
+      typeof messageOrOptions === 'string'
+        ? messageOrOptions
+        : getStatusMessage(statusCode)
+
+    super(statusCode, {
       message,
-      cause: options.cause
+      cause: resolvedOptions.cause
     })
 
+    this.statusCode = statusCode
     this.name = 'AppError'
-    this.errors = options.errors
+    this.errors = resolvedOptions.errors
   }
 }
 
