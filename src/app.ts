bec816c4b8729fc22706b0733f65e98362f04a9b
Rafi Ferdos <rafiferdos@gmail.com>
2026-09-09 00:30:38 +0600

    feat: add authentication routes to the API

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import {
   notFoundHandler
 } from './app/middlewares/globalErrorHandler'
 import { factory } from './factory'
+import { AuthRoutes } from './app/module/auth/auth.route'
 
 const app = factory.createApp()
 
@@ -23,6 +24,8 @@ app.get('/', (c) => {
     message: 'Welcome to LumaCare API'
   })
 })
+
+app.route('/api/v1/auth', AuthRoutes)
 
 app.notFound(notFoundHandler)
 app.onError(globalErrorHandler)
