7834f58d47c8d2f2b4a8045bea545300a9200f32
Rafi Ferdos <rafiferdos@gmail.com>
2026-09-08 20:36:05 +0600

    feat: update app to use Hono framework and add CORS middleware

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,60 @@
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import express, { type Application, type Request, type Response } from 'express'
-import httpStatus from 'http-status'
+// import cookieParser from 'cookie-parser'
+// import cors from 'cors'
+// import express, { type Application, type Request, type Response } from 'express'
+// import httpStatus from 'http-status'
 
-import config from './app/config'
-import globalErrorHandler from './app/middlewares/globalErrorHandler'
-import notFound from './app/middlewares/notFound'
-import { AuthRoutes } from './app/module/auth/auth.route';
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import config from "./app/config";
 
-const app: Application = express()
+// import config from './app/config'
+// import globalErrorHandler from './app/middlewares/globalErrorHandler'
+// import notFound from './app/middlewares/notFound'
+// import { AuthRoutes } from './app/module/auth/auth.route';
 
-app.use(
-  cors({
-    origin: config.frontend_url,
-    credentials: true
-  })
-)
+// const app: Application = express()
 
-// Enable URL-encoded form data parsing
-app.use(express.urlencoded({ extended: true }))
+// app.use(
+//   cors({
+//     origin: config.frontend_url,
+//     credentials: true
+//   })
+// )
 
-// Middleware to parse JSON bodies
-app.use(express.json())
-app.use(cookieParser())
+// // Enable URL-encoded form data parsing
+// app.use(express.urlencoded({ extended: true }))
 
-app.use("/api/v1/auth", AuthRoutes)
+// // Middleware to parse JSON bodies
+// app.use(express.json())
+// app.use(cookieParser())
 
-// Basic route
-app.get('/', async (_req: Request, res: Response) => {
-  res.status(httpStatus.OK).json({
+// app.use("/api/v1/auth", AuthRoutes)
+
+// // Basic route
+// app.get('/', async (_req: Request, res: Response) => {
+//   res.status(httpStatus.OK).json({
+//     success: true,
+//     message: 'Welcome to PH Healthcare System Backend'
+//   })
+// })
+
+// app.use(globalErrorHandler)
+// app.use(notFound)
+
+// export default app
+
+const app = new Hono()
+
+app.use('*', cors({
+  origin: config.frontend_url,
+  credentials: true  
+}))
+
+app.get('/', (c)=> {
+  return c.json({
     success: true,
-    message: 'Welcome to PH Healthcare System Backend'
+    message: 'Welcome to LumaCare Backend'
   })
 })
-
-app.use(globalErrorHandler)
-app.use(notFound)
 
 export default app
